0d9dd5c381e3643c96ff7ad2def136e4d84a2547
bywura <bushragbadegesin@gmail.com>
2026-08-12 10:43:23 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Prompt and Alert
+
+// alert("Hi, Bushra here!");
+
+console.error("Double check for errors");
+console.warn("Are you sure you want to proceed?");
+
+// let grade = 80;
+// console.log(typeof grade);
+
+let newGrade = prompt("Enter your grade");
+let newMark =
+  newGrade >= 80
+    ? "You have passed this course"
+    : "You have failed this course";
+    alert(newMark)
