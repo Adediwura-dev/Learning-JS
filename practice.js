1c6b1eb95507caeda24c4ab727094d1eb4d07ae4
Bushra <bushragbadegesin@gmail.com>
2026-08-16 14:51:18 +0100

    practiced further with nested if

--- practice.js
+++ practice.js
@@ -1,6 +1,16 @@
 alert(5 + 6);
 
-function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello Dolly!";
-  document.getElementById("demo2").innerHTML = "How are you?";
+let age = 16;
+let text = "You can Not drive";
+
+if (age >= 18) {
+  text = "You can drive";
+  alert(text)
 }
+
+// function myFunction() {
+//   document.getElementById("demo1").innerHTML = "Hello Dolly!";
+//   document.getElementById("demo2").innerHTML = "How are you?";
+// }
+
+
